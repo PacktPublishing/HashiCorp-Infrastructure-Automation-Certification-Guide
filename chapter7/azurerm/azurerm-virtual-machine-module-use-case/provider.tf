3cad40a871457d5f3db172375110ff1551212a20
Ravi Mishra <inmishrar@gmail.com>
2020-12-27 18:11:22 +0530

    commit

--- chapter7/azurerm/azurerm-virtual-machine-module-use-case/provider.tf
+++ chapter7/azurerm/azurerm-virtual-machine-module-use-case/provider.tf
@@ -4,7 +4,7 @@
   storage_account_name = "terraformstg2345"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
-    access_key           = "KRqtJIA0Gp4oKBsElDU7RGNw0duWLw8Nb29CZ6iyVc21QU5mV60FEWJOrUnFd8qkqnb5V2rdpOkysL73xFQHcg=="
+    access_key           = "KRqtJIA0Gp4oKBsElDU7RGN..."
     }
 }
 provider "azurerm" {
