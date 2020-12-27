278074c728bb1b1130a1a824ff6ebab5430d481b
Ravi Mishra <inmishrar@gmail.com>
2020-12-27 16:34:08 +0530

    commit

--- chapter7/azurerm/azurerm-virtual-machine-module-use-case/provider.tf
+++ chapter7/azurerm/azurerm-virtual-machine-module-use-case/provider.tf
@@ -1,11 +1,7 @@
- terraform {
-  required_version = ">= 0.12" 
-  backend "azurerm" {
-  storage_account_name = "terraformstg2345"
-    container_name       = "terraform"
-    key                  = "terraform.tfstate"
-    }
-}
+terraform {
+ required_version = ">= 0.12"                  
+   }
+
 provider "azurerm" {
   version = "=2.20.0"
   features {}
