576d42aad20591c969f6013bc368e14417daa75f
Ravi Mishra <inmishrar@gmail.com>
2021-01-21 13:10:38 +0530

    commit

--- chapter9/gcp/modules/route/variables.tf
+++ chapter9/gcp/modules/route/variables.tf
@@ -2,10 +2,12 @@
 	Routes Variables
  *****************************************/
 variable "project_id" {
+  type = string
   description = "The ID of the project where the routes will be created"
 }
 
 variable "vpc_name" {
+  type = string
   description = "The name of the network where routes will be created"
 }
 
