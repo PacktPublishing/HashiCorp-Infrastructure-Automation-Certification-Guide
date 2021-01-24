11df0cd1e83a3ae1e207202a721d63d51f976063
Ravi Mishra <inmishrar@gmail.com>
2021-01-25 00:57:42 +0530

    commit

--- chapter9/aws/stacks/outputs.tf
+++ chapter9/aws/stacks/outputs.tf
@@ -4,7 +4,7 @@ output "subnet_id" {
 }
 output "s3_id" {
   description = "S3 bucket id"
-  value       = module.storage.s3_id
+  value       = module.s3.s3_id
 }
 output "vpc_id" {
   description = "VPC id"
