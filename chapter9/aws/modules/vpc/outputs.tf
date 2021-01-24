273007d7382be14f16ca20b1477d7f0da414cd6f
Ravi Mishra <inmishrar@gmail.com>
2021-01-25 01:25:43 +0530

    commit

--- chapter9/aws/modules/vpc/outputs.tf
+++ chapter9/aws/modules/vpc/outputs.tf
@@ -2,8 +2,8 @@
 	VPC Code
  *****************************************/
 output "vpc_id" {
-  value = aws_vpc.vpc[count.index].id
+  value = aws_vpc.vpc.*.id
 }
 output "vpc_arn" {
-  value = aws_vpc.vpc[count.index].arn
+  value = aws_vpc.vpc.*.arn
 }
