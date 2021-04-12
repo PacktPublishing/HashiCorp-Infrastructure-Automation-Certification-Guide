71fefd7161afb6b75b294ab4bef4d8392c30ca5d
Ravi Mishra <inmishrar@gmail.com>
2021-04-12 11:52:03 +0530

    commit

--- chapter5/AWS/terraform.tfvars
+++ chapter5/AWS/terraform.tfvars
@@ -1,1 +1,2 @@
 cidr_block = "10.0.0.0/16"
+aws_region = "ap-southeast-1"
--- chapter5/AWS/variables.tf
+++ chapter5/AWS/variables.tf
@@ -1,3 +1,6 @@
 variable "cidr_block" {
   description = "provide VPC range"
 }
+variable "aws_region" {
+  description = "provide AWS region"
+}
