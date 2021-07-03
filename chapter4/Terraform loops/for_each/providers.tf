6e517de8c181842a9e6db6c72e68712d3de2879a
Ravi Mishra <inmishrar@gmail.com>
2021-07-03 19:06:11 +0530

    updated providers.tf in chapter4

--- chapter4/Terraform loops/for_each/providers.tf
+++ chapter4/Terraform loops/for_each/providers.tf
@@ -1,4 +1,12 @@
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.55.0"
+    }
+  }
+}
 provider "azurerm" {
-  version = "=2.55.0"
   features {}
 }
