21743be05049fdc24756e0c36637fe79ad962521
Ravi Mishra <inmishrar@gmail.com>
2021-07-08 02:17:28 +0530

    updated gcp subnet modules

--- chapter7/aws/aws-vpc-subnet-module-use-case/output.tf
+++
@@ -1,6 +0,0 @@
-output "vpc_id" {
-  value = module.terraform-aws-vpc.vpc_id
-}
-output "vpc_cidr_block" {
-  value = module.terraform-aws-vpc.vpc_cidr_block
-}
--- chapter8/Override-Files/override.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "rgname" {
-  tags = {
-    "environment" = "preprod"
-    "costcenter"  = "C3478"
-  }
-}
