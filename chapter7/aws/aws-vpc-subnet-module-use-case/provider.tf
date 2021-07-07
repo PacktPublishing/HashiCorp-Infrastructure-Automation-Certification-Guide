3d255cc1db4f2ec989c6bec6459a6547d7c5c148
Ravi Mishra <inmishrar@gmail.com>
2021-07-08 01:01:14 +0530

    aws-module-update

--- chapter7/aws/aws-vpc-subnet-module-use-case/provider.tf
+++ chapter7/aws/aws-vpc-subnet-module-use-case/provider.tf
@@ -1,5 +1,6 @@
 
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -8,5 +9,5 @@ terraform {
   }
 }
 provider "aws" {
-    region  = var.region
+  region = var.region
 }
