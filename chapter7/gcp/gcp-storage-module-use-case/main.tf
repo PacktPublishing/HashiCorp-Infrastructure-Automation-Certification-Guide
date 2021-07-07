be9a36352487f66fa634c9d1b4ca6cd672c0a409
Ravi Mishra <inmishrar@gmail.com>
2021-07-08 01:11:55 +0530

    updated gcp modules

--- chapter7/gcp/gcp-storage-module-use-case/main.tf
+++
@@ -1,9 +0,0 @@
-module "terraform-gcp-gcs" {
-  source      = "github.com/PacktPublishing/Hashicorp-Infrastructure-Automation-.git//chapter7/gcp/gcp-storage-module?ref=v2.0.0"
-  gcp_stg_name  = var.gcp_stg_name
-  gcp_location  = var.gcp_location
-  force_destroy = var.force_destroy
-  storage_class = var.storage_class
-  project       = var.project
-  labels        = var.labels
-}
