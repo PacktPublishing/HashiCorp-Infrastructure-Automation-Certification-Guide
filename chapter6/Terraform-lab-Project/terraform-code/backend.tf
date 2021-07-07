6e729a5444c08d237e5daecad499c7b91fa67818
Ravi Mishra <inmishrar@gmail.com>
2021-07-08 00:13:55 +0530

    commit

--- chapter6/Terraform-lab-Project/terraform-code/backend.tf
+++ chapter6/Terraform-lab-Project/terraform-code/backend.tf
@@ -1,5 +1,5 @@
  terraform {
-  required_version = ">= 0.12" 
+  required_version = ">= 1.0" 
  backend "azurerm" {
   storage_account_name = "__terraformstorageaccount__"
     container_name       = "terraform"
--- chapter7/azurerm/terraform-azurevm-module/provider.tf
+++ chapter7/azurerm/terraform-azurevm-module/provider.tf
@@ -1,5 +1,5 @@
  terraform {
-  required_version = ">= 0.12" 
+  required_version = ">= 1.0" 
   backend "azurerm" {
   storage_account_name = "terraformstg2345"
     container_name       = "terraform"
@@ -7,6 +7,6 @@
     }
 }
 provider "azurerm" {
-  version = "=2.20.0"
+  version = "=2.55.0"
   features {}
 }
